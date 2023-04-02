4acf0a546b893ce6306b59149a9b37b4fe9b1dab
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2023-04-02 13:00:20 +0200

    ej 3 arreglado y ej 4 comentado

--- Tema7EjerciciosPOO2/src/ejercicio3/Fecha.java
+++ Tema7EjerciciosPOO2/src/ejercicio3/Fecha.java
@@ -1,8 +1,8 @@
 package ejercicio3;
 
 /**
- * clase que contendra metodos para establecer y modificar fechas, compobrobar
- * si la fecha es correcta y mostrar la fecha por pantalla
+ * clase que contendra metodos para establecer y modificar fechas, comprobar si
+ * la fecha es correcta y mostrar la fecha por pantalla
  * 
  * @author juanpesca19
  *
@@ -51,10 +51,42 @@ public class Fecha {
 	 * @param dia contendra valor a asignar a atributo dia
 	 */
 	public void setDia(int dia) {
-		// si valor parametro de entrada dia es mayor a 0 y menor a 31
-		if (dia > 0 && dia < 32) {
-			// le doy su valor a atributo dia
-			this.dia = dia;
+
+		// hago switch de mes
+		switch (mes) {
+
+		// si mes es uno de los listados en el case (de los que tienen 31 dias)
+		case 1, 3, 5, 7, 8, 10, 12 -> {
+			// dia debera tener como minimo valor 1 y como maximo valor 31
+			if (dia > 0 && dia < 32) {
+				// de cumplirse la condicion modifico atributo dia con parametro de entrada dia
+				this.dia = dia;
+			}
+		}
+		// si mes es 2
+		case 2 -> {
+
+			// compruebo si el año es bisiesto y dia tiene valor entre 1 y 29
+			if ((esBisiesto()) && (dia > 0 && dia < 30)) {
+				// de cumplirse la condicion modifico atributo dia con parametro de entrada dia
+				this.dia = dia;
+
+				// si el año no es bisiesto compruebo si dia tiene valor entre 1 y 28
+			} else if (dia > 0 && dia < 29) {
+				// de cumplirse la condicion modifico atributo dia con parametro de entrada dia
+				this.dia = dia;
+			}
+
+		}
+
+		// si mes es uno de los listados en el case (de los que tienen 30 dias)
+		case 4, 6, 9, 11 -> {
+			// compruebo si el año es bisiesto y dia tiene valor entre 1 y 30
+			if (dia > 0 && dia < 31) {
+				// de cumplirse la condicion modifico atributo dia con parametro de entrada dia
+				this.dia = dia;
+			}
+		}
 		}
 	}
 
@@ -129,17 +161,20 @@ public class Fecha {
 	public boolean fechaCorrecta() {
 		boolean resultado = false;
 
-		// si el año es bisiesto el mes es febrero (2) y el dia es menor o igual a 29
+		// si el año es bisiesto el mes es febrero (2) y el dia ser menor o igual a 29
 		if (esBisiesto() && this.mes == 2 && this.dia <= 29) {
 			// resultado toma valor true
 			resultado = true;
 
-			// de no cumplirse lo anterior
+			// de no ser bisiesto pero el mes ser febrero (2) y el dia ser menor o igual a
+			// 28
 		} else if (this.mes == 2 && this.dia <= 28) {
 			// resultado toma valor true
 			resultado = true;
 
-		} else if (this.mes != 2 && this.dia > 0 && this.dia < 32) {
+			// de no cumplirse lo anterior ser el mes cualquiera menos el 2 y el dia ser
+			// menor o igual a 31
+		} else if (this.mes != 2 && this.dia < 32) {
 			resultado = true;
 		}
 		return resultado;
@@ -150,79 +185,111 @@ public class Fecha {
 	 */
 	public void diaSiguiente() {
 
-		// switch de mes
-
+		// hago switch de mes
 		switch (mes) {
-		case 1, 3, 5, 7, 8, 10, 12 -> {
 
+		// si mes es uno de los listados en el case (de los que tienen 31 dias)
+		case 1, 3, 5, 7, 8, 10, 12 -> {
+			// compruebo si estoy en el ultimo dia del mes
 			if (dia == 31) {
+				// de estarlo cambio valor de dia a 1
 				dia = 1;
+				// y sumo a valor de mes +1
 				mes++;
+				// y luego compruebo si el mes ha pasado a ser 13
+				if (mes == 13) {
+					// de serlo cambio valor de mes a 1
+					mes = 1;
+					// cambio valor de dia a 1
+					dia = 1;
+					// y sumo+1 a año
+					año++;
+				}
+
+				// si no estoy en el ultimo dia del mes
+			} else {
+				// sumo a dia +1
+				dia++;
 			}
 		}
 
+		// si mes es febrero (2)
 		case 2 -> {
-
+			// compruebo si año es bisiesto
 			if (esBisiesto()) {
+				// de serlo compruebo si estoy en el ultimo dia del mes
 				if (dia == 29) {
+					// estarlo doy a dia valor igual a 1
 					dia = 1;
-					mes++;
-					
-				} else if (dia == 28) {
-					dia = 1;
+					// y sumo +1 a mes
 					mes++;
 				}
-			}
-		}
 
-		case 4, 6, 9, 11 -> {
-			if (dia == 30) {
+				// de no estar en el ultimo dia del mes
+				else {
+					// sumo +1 a dia
+					dia++;
+				}
+				//de no ser año bisiesto compruebo si estoy en el ultimo dia
+			} else if (dia == 28) {
+				// de estarlo doy a dia valor igual a 1
 				dia = 1;
+				// y sumo +1 a mes
 				mes++;
+
+				// si no estoy en el ultimo dia del mes
+			} else {
+				// sumo +1 a dia
+				dia++;
 			}
 		}
 
-		case 13 -> {
-			mes = 1;
-			dia = 1;
-		}
-		}
-		if (esBisiesto() && this.dia < 29) {
-			this.dia++;
+		// si mes es uno de los listados en el case (de los que tienen 30 dias)
+		case 4, 6, 9, 11 -> {
+			// compruebo si estoy en el ultimo dia del mes
+			if (dia == 30) {
+				// de estarlo cambio valor de dia a 1
+				dia = 1;
+				// y sumo a valor de mes +1
+				mes++;
 
-		} else if (this.dia < 28) {
-			this.dia++;
+				// si no estoy en el ultimo dia del mes
+			} else {
+				// sumo a dia +1
+				dia++;
+			}
 		}
-		// hacer algo mas para los dias si dia es 30 0 31 si no es febrero?
-
-		// si atributo mes toma valor mayor a 13
-		if (this.mes > 12) {
-			// doy a atributo mes valor igual a 0
-			this.mes = 1;
-			// y sumo a atributo año valor +1
-			this.año++;
 		}
 	}
 
 	/**
-	 * metodo que devolvera una cadena en mensaje por pantalla con los valores de
-	 * los atributos del objeto que lo llame
+	 * metodo que devolvera una cadena en mensaje personalizado por pantalla con los
+	 * valores delos atributos del objeto que lo llame
 	 */
 	@Override
 	public String toString() {
 		String cadena = "";
 
-		if (this.dia < 10 && mes < 10) {
-			cadena += "0" + dia + "/" + "0" + mes + "/" + año;
-
-		} else if (this.dia < 10) {
-			cadena += "0" + dia + "/" + mes + "/" + año;
-
-		} else if (this.mes < 10) {
-			cadena += +dia + "/" + "0" + mes + "/" + año;
+		// si dia tiene valor menor a 10 le añado 0 delante, valor dia y barra a la
+		// cadena
+		if (this.dia < 10) {
+			cadena += "0" + dia + "/";
+			// de lo contrario solo valor dia y barra
 		} else {
-			cadena += +dia + "/" + mes + "/" + año;
+			cadena += dia + "/";
 		}
+
+		// si mes tiene valor menor a 10 le añado 0 delante, valor mes y barra a la
+		// cadena
+		if (this.mes < 10) {
+			cadena += "0" + mes + "/";
+			// de lo contrario solo valor mes y barra
+		} else {
+			cadena += mes + "/";
+		}
+
+		// añado valor año a la cadena
+		cadena += año;
 
 		return cadena;
 	}
--- Tema7EjerciciosPOO2/src/ejercicio3/Main.java
+++ Tema7EjerciciosPOO2/src/ejercicio3/Main.java
@@ -14,23 +14,36 @@ public class Main {
 		// creo escaner y lo nombro dogma
 		Scanner dogma = new Scanner(System.in);
 
-		// solicito a usuario valor para dia
-		System.out.println("Introduzca valor para dia: ");
-		// recojo dicho valor por escaner y lo asigno a valorSet
-		valorSet = dogma.nextInt();
-		// llamo al setter de atributo dia con objeto fecha dandole como parametro de
-		// entrada valorSet
-		fecha.setDia(valorSet);
-		// reinicio valor de valorSet a 0 para siguiente uso
-		valorSet = 0;
+		// empiezo solicitando valor para año, mes y luego dia porque setter de dia usa
+		// mes para hacer diferentes operaciones segun el valor de mes en switch
 
 		// solicito a usuario valor para mes
 		System.out.println("Introduzca valor para mes: ");
 		// recojo dicho valor por escaner y lo asigno a valorSet
 		valorSet = dogma.nextInt();
+		// si el valor de valorSet es menor a 1 o mayor a 12
+		if (valorSet < 1 || valorSet > 12) {
+			// imprimo mensaje de error por pantalla
+			System.out.println("Asegurese de introducir un valor para mes entre 1 y 12.");
+		}
 		// llamo al setter de atributo mes con objeto fecha dandole como parametro de
 		// entrada valorSet
 		fecha.setMes(valorSet);
+		// reinicio valor de valorSet a 0 para siguiente uso
+		valorSet = 0;
+
+		// solicito a usuario valor para dia
+		System.out.println("Introduzca valor para dia: ");
+		// recojo dicho valor por escaner y lo asigno a valorSet
+		valorSet = dogma.nextInt();
+		// si el valor de valorSet es menor a 1 o mayor a 31
+		if (valorSet < 1 || valorSet > 31) {
+			// imprimo mensaje de error por pantalla
+			System.out.println("Asegurese de introducir un valor para dia entre 1 y 31.");
+		}
+		// llamo al setter de atributo dia con objeto fecha dandole como parametro de
+		// entrada valorSet
+		fecha.setDia(valorSet);
 		// reinicio valor de valorSet a 0 para siguiente uso
 		valorSet = 0;
 
@@ -38,6 +51,11 @@ public class Main {
 		System.out.println("Introduzca valor para año: ");
 		// recojo dicho valor por escaner y lo asigno a valorSet
 		valorSet = dogma.nextInt();
+		// si el valor de valorSet es menor 0
+		if (valorSet < 0) {
+			// imprimo mensaje de error por pantalla
+			System.out.println("Asegurese de introducir un valor para año mayor a 0.");
+		}
 		// llamo al setter de atributo año con objeto fecha dandole como parametro de
 		// entrada valorSet
 		fecha.setAño(valorSet);
@@ -64,11 +82,23 @@ public class Main {
 			System.out.println("La fecha es incorrecta.");
 		}
 
-		// llamo al metodo diaSiguiente con el objeto fecha
+		// llamo al metodo diaSiguiente con el objeto fecha para avanzar un dia
 		fecha.diaSiguiente();
 
-		// muestro por pantalla llamo al metodo toString con el objeto fecha
+		// muestro mensaje por pantalla devuelto al llamar al metodo toString con el
+		// objeto fecha
 		System.out.println(fecha.toString());
+
+		// si el metodo fechaCorrecta llamado con objeto fecha devuelve true
+		if (fecha.fechaCorrecta()) {
+			// imprimo mensaje por pantalla para indicar que la fecha es correcta
+			System.out.println("La fecha es correcta.");
+
+			// de lo contrario
+		} else {
+			// imprimo mensaje por pantalla para indicar que la fecha no es correcta
+			System.out.println("La fecha es incorrecta.");
+		}
 
 		// cierro escaner
 		dogma.close();
